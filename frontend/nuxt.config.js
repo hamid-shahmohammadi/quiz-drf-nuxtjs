a158d92cb0ff2c0826d5cba58af3c904f023be73
unknown <hamid.shahmohamadi@gmai.com>
2021-12-22 16:17:49 +0330

    00-10-1-1617

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -23,7 +23,9 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~assets/css/bootstrap.min'
+    '~assets/css/bootstrap.min',
+    // '~assets/css/bootstrap.rtl.min',
+
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
